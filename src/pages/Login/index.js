ec8b72a5a3964be096107f5850f91b499f9bb24e
Karen Freitas <karenfcorrea@gmail.com>
2021-09-07 14:00:19 -0300

    Logando com dados de usuário

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,30 +1,70 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import {useHistory} from 'react-router-dom'
 
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button } from 'react-bootstrap';
 
 import logo from '../../img/logo.png';
 import background from '../../img/bg-login3.png';
 
-
-
 import './login.css'
+import { loginRedirection } from '../../services';
 
-const Login = () =>{
+export default function Login() {
+
+  const url = 'https://lab-api-bq.herokuapp.com/auth';
+
+  const history = useHistory();
+
+  const [values, setValues] = useState({ email: '', password: '' });
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+    console.log(values)
+  }
+
+
+
 
   const [validated, setValidated] = useState(false);
 
   const handleSubmit = (event) => {
+    event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
     }
 
     setValidated(true);
-  };
 
-  return(
+    const loginData = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+        values
+      )
+    };
+
+    fetch(url, loginData)
+      .then((response) => response.json())
+      .then((userData)=>{
+        console.log(userData);
+        localStorage.setItem('name', userData.name);
+        localStorage.setItem('token', userData.token);
+        localStorage.setItem('role', userData.role);
+        return userData.role;
+      })
+      .then((role) => {
+        loginRedirection(role, history)
+       })
+      
+  }
+
+  return (
     <>
       <div className="container-bg">
         <img src={background} className="bg-login" alt="background"></img>
@@ -35,14 +75,14 @@ const Login = () =>{
         <h2 className="mb-4" >LOGIN</h2>
         <Form Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group className="mb-4" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Email" required />
+            <Form.Control type="email" placeholder="Email" onChange={onChange} value={values.email} name="email" required />
             <Form.Control.Feedback type="invalid">
               Email inválido.
             </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-4" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Senha" required/>
+            <Form.Control type="password" placeholder="Senha" onChange={onChange} value={values.password} name="password" required />
             <Form.Control.Feedback type="invalid">
               Senha inválida.
             </Form.Control.Feedback>
@@ -52,17 +92,17 @@ const Login = () =>{
               ENTRAR
             </Button>
           </div>
-          <div  className="mb-5 d-flex justify-content-center">
+          <div className="mb-5 d-flex justify-content-center">
             <p className="text">PRIMEIRO ACESSO?</p>
-            <a  href="/cadastro" className=" link d-inline-flex">CADASTRE-SE</a>
+            <a href="/cadastro" className=" link d-inline-flex">CADASTRE-SE</a>
           </div>
         </Form>
       </div>
-     
-      
+
+
     </>
   )
-  
-};
 
-export default Login;
+}
+
+
