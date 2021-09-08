413bf8c3c740d61acd767f7defb85c525910afce
Isisnoron <isisbeatrizrn@gmail.com>
2021-09-08 18:44:35 -0300

    config: react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1> Hello Word</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
