5582f61673110aa88b4e9cf5b221426431a9d2a8
jjullimayanne <julli.souza@ufob.edu.br>
2021-09-08 14:41:28 -0300

    api registro e login

--- src/components/router.js
+++ src/components/router.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import Kitchen from "../pages/kitchen";
-import Login from '../pages/login'
+import Login from '../pages/Login/login'
+import Registro from '../pages/Registro/registro'
 import Pedidos from "../pages/pedidosPages";
 
 function Routes() {
     return(
         <Switch>
-            <Route path="/1" component={Login}></Route>
-            <Route path="/" component={Kitchen}></Route>
-            <Route path="/3"component={Pedidos}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/cozinha" component={Kitchen}></Route>
+            <Route path="/salao"component={Pedidos}></Route>
+            <Route path="/registrar"component={Registro}></Route>
+
 
         </Switch>
     )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/router'
 import {BrowserRouter} from 'react-router-dom'
+import './index.css';
 
 ReactDOM.render(
     
