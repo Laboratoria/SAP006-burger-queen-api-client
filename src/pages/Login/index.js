e9d1390a522e109729b7fbf3eca6f2f1254ec1aa
Isisnoron <isisbeatrizrn@gmail.com>
2021-09-13 16:18:52 -0300

    css responsivo login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,31 +1,39 @@
 import Button from "../../components/Button/button.js";
 import Input from "../../components/inputs/index.js";
 import { Link } from "react-router-dom";
+import "./style.css";
+import logoMagic from "../../img/logoMagic.png";
 
 function Login() {
   return (
-    <form>
-      <form className="input-login">
-        <Input placeholder="Email"></Input>
-        <Input placeholder="Senha"></Input>
-      </form>
-      <div className="btn-container">
+    <form className="container">
+      {/* <div className="container"> */}
+      <img src={logoMagic} className="logoMagic" alt="logo" />
+
+      <div className="container-login">
+        <form className="input-login">
+          <Input className="input-email" placeholder="Email"></Input>
+          <Input placeholder="Senha"></Input>
+        </form>
+
         <Button
           btnId="btn-enter"
           btnType="button"
           btnClass="goBtn"
           btnText="Entrar"
         />
-        <p className="new-worker">É um novo funcionário?</p>
+        {/* <hr> </hr> */}
+        <p className="new-worker">Funcionário novo?</p>
         <Link to="/register">
           <Button
             btnId="btn-register"
             btnType="button"
-            btnClass="goBtn"
+            btnClass="registerBtn"
             btnText="Cadastre-se"
           />
         </Link>
       </div>
+      {/* </div> */}
     </form>
   );
 }
