54e083053d935d9ffd7ffe98734ea2b0c5a39588
Isisnoron <isisbeatrizrn@gmail.com>
2021-09-14 21:09:58 -0300

    validação de erros pag Cadastro

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom';
-import '../inputs/style.css';
+import "../inputs/style.css";
 
-const Input = ({ placeholder }) => {
-    return <input type="text" placeholder={placeholder} className="input"></input>
-}
+const Input = ({ name, type, placeholder, value, onChange }) => {
+  return (
+    <input
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      className="input"
+      onChange={onChange}
+    />
+  );
+};
 
 export default Input;
--- src/pages/signUp/index.js
+++ src/pages/signUp/index.js
@@ -1,31 +1,74 @@
-import  Input  from '../../components/inputs/index.js';
-import Button from '../../components/Button/button.js';
-import '../signUp/style.css';
+import Input from "../../components/inputs/index.js";
+import Button from "../../components/Button/button.js";
+import "../signUp/style.css";
 import logoMagic from "../../img/logoMagic.png";
+import ValueAndError from "./formValueAndError.js";
+import validateValues from "./ValidateRegister.js";
 
 function alertApears() {
-  alert("clicouuu");
+  // alert("clicouuu");
 }
 
-const signUp = () => {
-    return (
+export function signUp() {
+  const { handleChange, values, handleSubmit, errors } = ValueAndError(validateValues);
+
+  return (
     <main>
-    <img alt="" src={logoMagic}></img>
-    <form className="form">
-      <h1>Bem Vindo!</h1>
-      <section className="form-input"> 
-      <Input placeholder="Nome"></Input>
-      <Input placeholder="Email"></Input>
-      <Input placeholder="Senha"></Input>
-      </section>
-      <hr width="150px"></hr>
-      <p>Selecione seu cargo:</p>
-      <label><input type="radio" name="workingOptions" value="Atendente"></input>Atendente</label>
-      <label><input type="radio" name="workingOptions" value="Cozinheiro(a)"></input>Cozinheiro(a)</label>
-      <Button btnClass="createAccount" btnText="Cadastrar" btnOnClick={alertApears} />
-    </form>
+      <img alt="" src={logoMagic}></img>
+      <form className="form" onSubmit={handleSubmit}>
+        <h1>Bem Vindo!</h1>
+        <section className="form-input">
+          {errors.name && <p>{errors.name}</p>}
+          <Input
+            name="name"
+            type="text"
+            placeholder="Nome"
+            autoComplete="off"
+            value={values.name}
+            onChange={handleChange}
+          />
+
+          {errors.email && <p>{errors.email}</p>}
+          <Input
+            name="email"
+            id="email"
+            type="text"
+            placeholder="Email"
+            autoComplete="off"
+            value={values.email}
+            onChange={handleChange}
+          />
+
+          {errors.password && <p>{errors.password}</p>}
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Senha"
+            autoComplete="off"
+            value={values.password}
+            onChange={handleChange}
+          />
+        </section>
+
+        <hr className="line" />
+        <p>Selecione seu cargo:</p>
+        <label>
+          <input type="radio" name="workingOptions" value="atendente" />
+          Atendente
+        </label>
+        <label>
+          <input type="radio" name="workingOptions" value="cozinheiro" />
+          Cozinheiro(a)
+        </label>
+        <Button
+          btnClass="createAccount"
+          btnText="Cadastrar"
+          btnOnClick={alertApears}
+        />
+      </form>
     </main>
-    );
+  );
 }
 
 export default signUp;
