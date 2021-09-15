56fcc9c8e55f77cacc89bade844132d1fad109a6
Karen Freitas <karenfcorrea@gmail.com>
2021-09-15 17:29:04 -0300

    ajustando o botão complexo e carrinho

--- src/components/LinkAside/index.js
+++ src/components/LinkAside/index.js
@@ -1,19 +1,26 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import './linkAside.scss'
 
 
-export default function LinkAside() {
+export default function LinkAside({
+ children, className, type 
+}) {
   return (
     <li>
       <a href>
-      <div className="item-aside burger">
-        <div className="border-link">
-          <span>LANCHES</span>
-        </div>  
-          
-      </div>
+        <div className={`item-aside ${type} ${className}`}>
+         
+        </div>
       </a>
+      <div className={`effects ${className}`}>
+        <div className={`border-link ${className}`}>
+          <span>{children}</span>
+        </div>
+        <div className="underline"></div>
+
+      </div>
     </li>
-    
+
   )
 }
