393379f61d3afd96d29474d68eddbdefb5026249
Karen Freitas <karenfcorrea@gmail.com>
2021-09-16 00:55:28 -0300

    alternando entre links do menu

--- src/pages/Hall/index.js
+++ src/pages/Hall/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import ButtonLogout from '../../components/ButtonLogout';
 import LinkAside from '../../components/LinkAside';
@@ -13,21 +13,111 @@ import ButtonRadio from '../../components/ButtonRadio';
 
 
 export default function Hall() {
-  const [breakfastClass, setBreakfastClass] = useState("")
+	const [breakfastClass, setBreakfastClass] = useState("")
 	const [allDayClass, setAllDayClass] = useState("selected")
 
-	const [activeBurgers, setActiveBurgers] = useState(true)
-	const [activeSides, setActivSactiveSides] = useState(false)
-	const [activeDrinks, setActiveDrinks] = useState(false)
+	const [values, setValues] = useState({
+		first: 'burgers',
+		second: 'sides',
+		third: 'drinks',
+	})
 
-	const selectBreakfast = () =>{
+	const [labels, setLabels] = useState({
+		first: 'burgers',
+		second: 'adicionais',
+		third: 'bebidas',
+	})
+
+	// const [activeBurgers, setActiveBurgers] = useState(true)
+	// const [activeSides, setActiveSides] = useState(false)
+	// const [activeDrinks, setActiveDrinks] = useState(false)
+	// const [activeSandwiches, setActiveSandwiches] = useState(true)
+	// const [activeCoffes, setActiveCoffes] = useState(false)
+	// const [activeJuices, setActiveJuices] = useState(false)
+
+	const [activation1, setActivation1] = useState(true)
+	const [activation2, setActivation2] = useState(false)
+	const [activation3, setActivation3] = useState(false)
+
+	const[mensagem, setMensagem] = useState("Burgers")
+
+
+
+	const selectBreakfast = () => {
+		setActivation1(true)
+		setActivation2(false)
+		setActivation3(false)
 		setBreakfastClass("selected")
 		setAllDayClass("")
+		setValues({
+			first: 'sandwiches',
+			second: 'coffes',
+			third: 'juices',
+		})
+		setLabels({
+			first: 'lanches',
+			second: 'cafés',
+			third: 'sucos',
+		})
 	}
 
-	const selectAllDay = () =>{
+	const selectAllDay = () => {
+		setActivation1(true)
+		setActivation2(false)
+		setActivation3(false)
 		setAllDayClass("selected")
 		setBreakfastClass("")
+		setValues({
+			first: 'burgers',
+			second: 'sides',
+			third: 'drinks',
+		})
+		setLabels({
+			first: 'burgers',
+			second: 'adicionais',
+			third: 'bebidas',
+		})
+	}
+
+	const changeProducts = (e) => {
+		const click = e.target.innerText
+		switch (click) {
+			case "BURGERS":
+				setActivation1(true)
+				setActivation2(false)
+				setActivation3(false)
+				break;
+			case "ADICIONAIS":
+				setActivation1(false)
+				setActivation2(true)
+				setActivation3(false)
+				setMensagem("Produtos Adicionais")
+				break
+			case "BEBIDAS":
+				setActivation1(false)
+				setActivation2(false)
+				setActivation3(true)
+				break
+			case "LANCHES":
+				setActivation1(true)
+				setActivation2(false)
+				setActivation3(false)
+				break;
+			case "CAFÉS":
+				setActivation1(false)
+				setActivation2(true)
+				setActivation3(false)
+				break
+			case "SUCOS":
+				setActivation1(false)
+				setActivation2(false)
+				setActivation3(true)
+				break
+
+			default:
+		}
+
+
 	}
 
 
@@ -43,41 +133,42 @@ export default function Hall() {
 			</header>
 
 			<nav>
-					<ul className="menu-types">
-						<li className={breakfastClass} onClick={selectBreakfast}> Breakfast </li>
-						<li className={allDayClass} onClick={selectAllDay}> All Day </li>
-					</ul>
+				<ul className="menu-types">
+					<li className={breakfastClass} onClick={selectBreakfast}> Breakfast </li>
+					<li className={allDayClass} onClick={selectAllDay}> All Day </li>
+				</ul>
 
 
 				<div className="order-progress">
-				<ButtonDefault
-					className="btn-order-status btn-default"
-				>
-					Em andamento
-				</ButtonDefault>
+					<ButtonDefault
+						className="btn-order-status btn-default"
+					>
+						Em andamento
+					</ButtonDefault>
 
-				<ButtonDefault
-					className="btn-order-status btn-default margin-left-1"
-				>
-					Finalizados
-				</ButtonDefault>
+					<ButtonDefault
+						className="btn-order-status btn-default margin-left-1"
+					>
+						Finalizados
+					</ButtonDefault>
 				</div>
 			</nav>
 			<main>
 
 				<section className="order-filling">
 					<div className="client-data margin-bottom-2">
-						<Input className="input-hall" placeholder="Insira o nome do cliente"/>
+						<Input className="input-hall" placeholder="Insira o nome do cliente" />
 					</div>
 					<div className="menu">
 						<aside>
-							<LinkAside className={activeBurgers ? "active" : null} type="burger">Burgers</LinkAside>
-							<LinkAside className={activeSides ? "active" : null} type="sides">Adicionais</LinkAside>
-							<LinkAside className={activeDrinks ? "active" : null} type="drinks" >Bebidas</LinkAside>
+							<LinkAside className={activation1 ? "active" : null} type={values.first} onClick={changeProducts}>{labels.first}</LinkAside>
+							<LinkAside className={activation2 ? "active" : null} type={values.second} onClick={changeProducts}>{labels.second}</LinkAside>
+							<LinkAside className={activation3 ? "active" : null} type={values.third} onClick={changeProducts}>{labels.third}</LinkAside>
 						</aside>
 
 						<section className="products">
-							<div className="products-row">					
+							<p>{mensagem}</p>
+							<div className="products-row">
 								<ProductArea
 									inputId='simples'
 									inputName='burger-options'
@@ -102,7 +193,7 @@ export default function Hall() {
 							</div>
 
 							<div className="radio-wrapper">
-								<ButtonRadio 
+								<ButtonRadio
 									// onChange={onChange} 
 									firstInputName="drinks"
 									firstInputValue="water"
@@ -118,7 +209,7 @@ export default function Hall() {
 									thirdClassName="hidden"
 								/>
 
-								<ButtonRadio 
+								<ButtonRadio
 									// onChange={onChange} 
 									firstInputName="burger"
 									firstInputValue="carne"
