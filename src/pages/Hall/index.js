6c85ba96e61574d996245a988d4ef767ebe0b528
Karen Freitas <karenfcorrea@gmail.com>
2021-09-16 15:09:58 -0300

    vercel conflito

--- src/pages/Hall/index.js
+++ src/pages/Hall/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState} from 'react';
 
 import ButtonLogout from '../../components/ButtonLogout';
 import LinkAside from '../../components/LinkAside';
