38920207e06123f9117df5fbc980d44cf0f4c6f4
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-09-17 19:04:09 -0300

    Criando componentes do menu all-day

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Burgers from '../../components/All-day/Burgers';
+// import Sides from '../../components/All-day/Sides';
+// import Drinks from '../../components/All-day/Drinks';
+
 
 export default function Kitchen ()  {
 	return (
-	<h1>Cozinha</h1>
+		<>
+	<Burgers />
+	{/* <Sides />
+	<Drinks /> */}
+	</>
 	)
 };
 
