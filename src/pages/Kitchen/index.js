32de2eb14952ae88a4ced92b5c790e8040fd85c7
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-09-17 19:11:21 -0300

    Ajuste no componente

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import Burgers from '../../components/All-day/Burgers';
-// import Sides from '../../components/All-day/Sides';
-// import Drinks from '../../components/All-day/Drinks';
-
 
 export default function Kitchen ()  {
 	return (
-		<>
-	<Burgers />
-	{/* <Sides />
-	<Drinks /> */}
-	</>
+	<h1>Cozinha</h1>
 	)
 };
-
