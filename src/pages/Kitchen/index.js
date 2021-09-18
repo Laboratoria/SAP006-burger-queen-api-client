8fc300da55d6d78548d0d0681c17c6eaa6050d60
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-09-18 14:17:07 -0300

    Criando componente popup

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
-export default function Kitchen ()  {
+import Popup from '../../components/Popup';
+
+export default function Kitchen() {
+	const [showPopup, setShowPopup] = useState(false);
+
+	const openPopup = () => {
+		setShowPopup(true);
+	};
+
 	return (
-	<h1>Cozinha</h1>
-	)
-};
+		<>
+			<h1>Cozinha</h1>
+			<button
+				style={{
+					color: 'white',
+				}}
+				onClick={openPopup}
+			>
+				abrir
+			</button>
+
+			{showPopup ? (
+				<Popup
+					popupText="Selecione um item antes de adicioná-lo!"
+					onClose={() => setShowPopup(false)}
+				></Popup>
+			) : null}
+		</>
+	);
+}
