cf491416355876fb354addf2f822db83fdf7be4b
giuthamie <giumie@hotmail.com>
2021-09-20 09:58:07 -0300

    testes login, contemplando primeira historia de usuario apenas

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import useFormLogin from './useFormLogin';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 const Login = () => {
   const { handleChange, handleSubmit } = useFormLogin();
 
   return (
-    <div className='loginPage'>
-      <h2>Login</h2>
+    <Router>
+     <div className='login-page'>
+      <h2 className='login-title'>Login</h2>
       <form onSubmit={handleSubmit}>
         <input type='email' name='email' placeholder='exemplo@exemplo.com' onChange={handleChange} />
         <input type='password' name='password' placeholder='Senha' onChange={handleChange} />
@@ -17,8 +19,8 @@ const Login = () => {
       </form>
       <p className='new-user'>Não possuí cadastro?</p>
       <div><Link className="link" to='/signup'>Cadastre-se</Link></div>
-    </div>
-
+     </div>
+    </Router>
   );
 }
 
