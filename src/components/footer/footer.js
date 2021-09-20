36c6776375d122108a62f899c8fe1848b97b47ea
jenniferpessoa <jenni.pessoa@gmail.com>
2021-09-20 20:51:20 -0300

    Component Footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './footer.css';
+
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer class="footer">
+                <p> Desenvolvido por
+                    Jennifer Pessoa <a href="https://github.com/jenniferpessoa" target="_blank"> <i class="fab fa-github"></i></a> <a href="https://www.linkedin.com/in/jennifer-pessoa/" target="_blank" > <i class="fab fa-linkedin"></i></a> e
+                    Juliana Monteiro <a href="https://github.com/JulianaMonteiro4" target="_blank"> <i class="fab fa-github"></i></a> <a href="https://www.linkedin.com/in/-juliana-monteiro/" target="_blank" > <i class="fab fa-linkedin"></i></a>
+                </p>
+            </footer>
+        </div>
+
+    )
+}
+
