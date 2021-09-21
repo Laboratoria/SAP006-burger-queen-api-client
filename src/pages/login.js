bcdf7f5056a8be1f59795a95d766afc0da477989
Magna Dutra <magdutra.jobs@gmail.com>
2021-09-21 09:43:39 -0300

    Layout login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,42 @@
 import '../styles/login.css';
+import logo from '../img/logo.png'
+import { MdEmail, MdLock } from "react-icons/md";
 
 function login() {
     return (
         <div className="pagina-login">
             <header className="pagina-login-header">
-            
+                <img src={logo} alt="Logo Burguer Queen" />
             </header>
-            <main className="pagina-login-main">
+            <div className="pagina-login-main">
                 <h1>login</h1>
-            </main>
+                <form className="login-form">
+                    <div className="loginInputEmail">
+                        <MdEmail />
+                        <input
+                        type="text"
+                        placeholder="Digite um email"
+                        />
+                    </div>
+                    <div className="loginInputPassword">
+                        <MdLock />
+                        <input
+                            type="text"
+                            placeholder="Digite sua senha"
+                        />
+                    </div>
+                    <button type="submit">
+                        Entrar
+                    </button>
+                    
+                    <h2>Não tenho conta!</h2>
+
+                    <button type="submit">
+                        Cadastre-se
+                    </button>
+                </form>
+            
+            </div>
         </div>
     );
 }
