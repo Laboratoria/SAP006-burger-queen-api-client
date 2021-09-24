09ebe37344cc79bf7df8f7f343371922e5aef009
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-09-23 21:20:08 -0300

    Adicionando botão de update orders

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ListAllOrders from '../../components/ListAllOrders';
 import Header from '../../components/Header'
+import ButtonDefault from '../../components/ButtonDefault';
+
+import './kitchen.scss';
+
 
 export default function Kitchen() {
+
+	const handleUpdateOrders = () => {
+		window.location.reload()
+	}
+
 	return (
 		<div className="pages-container">
-			<Header/>
+			<Header />
+
+			<ButtonDefault className="btn-update-orders" onClick={() => { handleUpdateOrders() }}>
+				🔔 Atualizar pedidos
+			</ButtonDefault>
+
 			<ListAllOrders />
 		</div>
 	);
