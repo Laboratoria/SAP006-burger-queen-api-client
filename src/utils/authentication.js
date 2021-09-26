d4ce40b0616248762edc2f339af671b41e3daccd
KamilaMoura1 <kamila.demoura93@gmail.com>
2021-09-26 15:16:12 -0400

    rotas privadas e api

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,4 +1,12 @@
-
+const isAuthenticated = () => {
+    const validation = localStorage.getItem(`token`)
+    if (validation !== undefined && validation !== null) {
+      return true
+    }
+    return false
+  }
+  
+  export default isAuthenticated
 
 
 
