347777eb7db9dce1f8733a2450d893e3d22eb79b
nessacl-95 <vanessalima951@gmail.com>
2021-09-26 22:04:47 -0300

    total price implemented and menu hooks refactor

--- src/pages/menu/useProducts.js
+++ src/pages/menu/useProducts.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 const useProducts = () => {
   const [products, setProducts] = useState([]);
   const [productsType, setProductsType] = useState('breakfast');
+  const [addItem, setAddItem] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const getData = async () => {
     const data = await getProducts().then(data => data)
@@ -14,11 +16,19 @@ const useProducts = () => {
     return getData()
   }, [])
 
+  useEffect(() => {
+    const sum = (previousValue, currentValue) => previousValue + currentValue;
+    setTotal(() => {
+      const price = addItem.map((elem) => elem.price);
+      return price.reduce(sum, 0)
+    })
+  }, [addItem])
+
   const handleButtonTypeClick = (e) => setProductsType(e.target.value);
 
-  const productsFiltered = products.filter((elem) => elem.sub_type.includes(productsType));
+  const productsFiltered = () => products.filter((elem) => elem.sub_type.includes(productsType));
 
-  return { handleButtonTypeClick, productsFiltered }
+  return { handleButtonTypeClick, productsFiltered, setAddItem, addItem, total }
 }
 
 export default useProducts;
