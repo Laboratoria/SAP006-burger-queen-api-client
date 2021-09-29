33d68b2c56e938259a882dd8778c8762fe04f527
Bianca Cristina Alves <83368857+biancacristinaalves@users.noreply.github.com>
2021-09-29 19:59:27 -0300

    heroku

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import notfound from '../../img/notfound.gif';
+// import notfound from '../../img/notfound.gif';
 
 import './NotFound.css';
 
@@ -8,7 +8,7 @@ export default function NotFound () {
 
         <div className="container-notfound">
           <h1> 404 ERROR - Página não encontrada</h1>
-          <img src={notfound} alt="counter" className="notfound"/>
+          {/* <img src={notfound} alt="counter" className="notfound"/> */}
         </div>
 
     );
