52740df269d500d0a32b42c7d83edddd4e6ed63f
bruna-devbio <bruna.ppp8@gmail.com>
2021-10-01 11:19:09 -0300

    WIP envio de pedidos para a cozinha

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from '../Input/Input'
 import Button from '../Button/Button';
 
+
 import './Cart.css'
 
-function Cart({ cartItem }) {
-	//aqui ele vai receber o parâmetro do remove
+function Cart({ cartItem, removeProducts }) {
+
+
+	const token = localStorage.getItem('userToken')
+	
+
+	const [valuesClient, setValuesClient] = useState({
+		client: '',
+		table: '',
+	})
+	console.log(valuesClient);
+
+	const ButtonSendOrder = () => {
+		
+		fetch('https://lab-api-bq.herokuapp.com/orders/', {
+				method: 'POST',
+				headers: {
+						accept: 'application/json',
+						Authorization: `${token}`,
+						'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: JSON.stringify({
+						client: 'Fulana',
+						table: 02,
+						products: [
+								{
+								 id: '',
+								 qtd: 1
+								}
+
+						]
+				})
+		})
+		.then((response) => response.json())
+		.then((json) => {
+				console.log(json);
+		})
+}
+
+
+
 	const totalPrice = cartItem.reduce((acumulador, item) => acumulador + item.price * item.qtd, 0)
-	console.log(cartItem)
+
+
+	const onChangeValues = (event) => {
+		setValuesClient({
+			...valuesClient,
+			[event.target.name]: event.target.value
+		})
+	}
 
 	return (
 		<div className='container-cart'>
@@ -16,31 +63,37 @@ function Cart({ cartItem }) {
 				<Input type='text'
 					placeholder='Nome'
 					className='data-clients-input'
-					name='email'>Cliente</Input>
+					value={valuesClient.client}
+					name='client'
+					onChange={onChangeValues}>Cliente</Input>
 				<Input type='text'
 					placeholder='Mesa'
+					value={valuesClient.table}
 					className='data-clients-input'
-					name='email'>Mesa</Input>
+					name='table'
+					onChange={onChangeValues}>Mesa</Input>
 			</div>
 
-		
-			<ul className='list'>
-				<li>Nome</li>
-				<li>Qntd.</li>
-				<li>Valor</li>
-			</ul>
-
-
-			{cartItem.map((item) => {
-				return (
-					<div className='order-list' key={item.id}>
-							<li className='name-item'>{item.name}</li>
-							<li className='qtd-item'>{item.qtd}</li>
-							<li className='price-item'>R$ {(item.price * item.qtd).toFixed(2)}</li>
-					</div>
-				)
-			})}
-
+			<table>
+				<tr className='order-list'>
+					<th>Nome</th>
+					<th>Qntd.</th>
+					<th>Valor</th>
+				</tr>
+			
+				{cartItem.map((item, index) => {
+					return (
+								<tr className='order-list' key={item.id}>
+					      <td className='name-item'>{item.name}</td>
+								<td className='qtd-item'>{item.qtd}</td>
+								<td className='price-item'>{(item.price * item.qtd).toFixed(2)}</td>
+								<td>
+								<button className='btn-remove' onClick={() => removeProducts(item)}>-</button>
+								</td>
+				        </tr>
+					)
+				})}
+			</table>
 			<div className='order-total'>
 				<div className='total-price'>
 					<p>Total:</p>
@@ -49,7 +102,8 @@ function Cart({ cartItem }) {
 				<div>
 					<Button
 						buttonText='Enviar pedido'
-						className='btn-send-order'/>
+						className='btn-send-order' 
+						buttonOnClick={() => ButtonSendOrder()}/>
 				</div>
 			</div>
 
@@ -58,3 +112,21 @@ function Cart({ cartItem }) {
 }
 
 export default Cart;
+
+<table>
+	<tr>
+		<th>Company</th>
+		<th>Contact</th>
+		<th>Country</th>
+	</tr>
+	<tr>
+		<td>Alfreds Futterkiste</td>
+		<td>Maria Anders</td>
+		<td>Germany</td>
+	</tr>
+	<tr>
+		<td>Centro comercial Moctezuma</td>
+		<td>Francisco Chang</td>
+		<td>Mexico</td>
+	</tr>
+</table>
