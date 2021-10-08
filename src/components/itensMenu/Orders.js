25807ce8951de2a411db7b3791c0ade18ca69922
Bianca Cristina Alves <83368857+biancacristinaalves@users.noreply.github.com>
2021-10-08 16:04:17 -0300

    aparecendo nome do cliente, status

--- src/components/itensMenu/Orders.js
+++ src/components/itensMenu/Orders.js
@@ -4,25 +4,53 @@ import Button from '../button/Button';
 import './Style.css';
 
 
-export default function Orders ( {id, client, table, status, name, qtd, Products} ) { 
+export default function Orders ( {id, client_name, table, status, createdAt,updatedAt,user_id } ) { 
+
+
+    /*const ordersFiltered = () => {
+        return orders.filter((item) => item.status === 'finalizado')
+      }
+    
+      const changeStatus = (elem) => {
+        updateOrderStatus('/orders/', elem.id, 'servido')
+          .then(() => setOrderStatus([...orderStatus, { id: elem.id, status: 'servido' }]))
+      }
+      
+      useEffect(() => {
+    return orderStatus.map((order) => {
+      const foundOrder = orders.map((elem) => elem).findIndex((item) => item.id === order.id)
+      if (foundOrder !== -1) {
+        console.log('remover')
+        const removed = orders
+        removed.splice(foundOrder, 1)
+        setOrders([...removed])
+        console.log(orders)
+      }
+      return orders
+    })
+  }, [orderStatus, orders])
+      */
+
 
     return(
         <div className="container-orders">
             <div className="container-infos-orders">
                 <p className="number-order">Nº do pedido:{id}</p>
-                <p className="clerk-order">Atendente: {localStorage.getItem("userName")} </p>
-                <p className="client-order">Cliente:{client}</p>
+                <p className="clerk-order">Atendente: {user_id} </p>
+                <p className="client-order">Cliente:{client_name}</p>
                 <p className="table-order">Mesa:{table}</p>
             </div>
             <div>
-                <p className="status-order">Status:{status}</p>
+                <h3>
+                    {" "}
+                    {status === "Status: Pendente" ? <span className="status-pending"> {status} </span > : <span className="status-preparing"> {status} </span> }             
+                </h3>
             </div>
-            <div>
-                <p className="itens-order">Produto:{name}</p> 
-                {/* fazer um map  
-                Products.map(() => ())*/}
-                <p className="qtd-order">Qtd: {qtd}</p>                    
-            </div>
+           
+            <aside className="orders-time">
+                <p>🕗 Realizado: {createdAt} </p>
+                <p>Atualizado às {updatedAt} </p>
+            </aside>
             <Button
                 text='Entregar Pedido'
                 className="btn-delivery"
