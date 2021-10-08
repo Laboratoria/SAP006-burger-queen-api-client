2ac871e2d7b670d1de6ee1f5aa31616f08df5fd0
Leonardo <leonardo_mbg@hotmail.com>
2021-10-08 18:50:51 -0300

    esqueleto

--- src/Pages/Register/register.js
+++ src/Pages/Register/register.js
@@ -1,26 +1,59 @@
-import React from "react";
+/* eslint-disable*/
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "../../Components/Button/button";
+import Input from "../../Components/input/input";
 
 const Register = () => {
+  const [userName, setUserName] = useState();
+  const [userPassword, setUserPassword] = useState();
+  const [userEmail, setUserEmail] = useState();
+
+  console.log(userName);
   const criarNovoUsuario = () => {
     console.log("cadastrar user");
   };
 
   return (
     <>
-      <h1>Cadastro</h1>
-      <form>
-        <input type="name" name="name" placeholder="Nome" />
-        <select name="role">
-          <option value="chef">Chefe</option>
-          <option value="waiter">Atendente</option>
-        </select>
-        <input type="email" name="email" placeholder="E-mail" />
-        <input type="password" name="password" placeholder="Senha" />
-        <button type="submit" onSubmit={criarNovoUsuario}>
-          entrar
-        </button>
-      </form>
+      <section className="container">
+        <div className="container-form">
+          <h1>Cadastro</h1>
+        </div>
+        <form className="formulario">
+          <Input
+            InputType="text"
+            inputPlaceholder=" Digite seu Nome"
+            inputValue={userName}
+            inputOnClick={(event) => setUserName(event.target.value)}
+          />
+
+          <Input
+            InputType="text"
+            inputPlaceholder=" Digite seu Email"
+            inputValue={userEmail}
+            inputOnClick={(event) => setUserEmail(event.target.value)}
+          />
+
+          <Input
+            InputType="password"
+            inputPlaceholder=" Senha"
+            inputValue={userPassword}
+            inputOnClick={(event) => setUserPassword(event.target.value)}
+          />
+          <select name="role">
+            <option value="chef">Chefe</option>
+            <option value="waiter">Atendente</option>
+          </select>
+
+          <Button
+            buttonClass="btn-form"
+            buttonOnClick={() => criarNovoUsuario()}
+          >
+            entrar
+          </Button>
+        </form>
+      </section>
       <Link to="/">Tenho Conta</Link>
     </>
   );
