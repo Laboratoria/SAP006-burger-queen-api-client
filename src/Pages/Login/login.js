3b2c2815427e464de6d12a3660d7600034249bb3
Leonardo <leonardo_mbg@hotmail.com>
2021-10-22 13:04:35 -0300

    layout pronto

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import './Login/login.css';
+//import "./login.css";
 
 const Login = () => {
   const logar = () => {};
@@ -8,14 +8,11 @@ const Login = () => {
   return (
     <>
       <h1>Login</h1>
-      <form>
-        <input type="email" name="email" placeholder="Digite o e-mail" />
-        <input type="password" name="password" placeholder="Senha" />
-        <button type="submit" onClick={logar}>
-          entrar
-        </button>
-        <Link to="/Cadastro">Cadastre aqui</Link>
-      </form>
+
+      <button type="submit" onClick={logar}>
+        entrar
+      </button>
+      <Link to="/Cadastro">Cadastre aqui</Link>
     </>
   );
 };
