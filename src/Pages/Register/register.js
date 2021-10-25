e38d81dd3504f872e5e5b9132eb28797d315ae72
Leonardo <leonardo_mbg@hotmail.com>
2021-10-25 18:03:35 -0300

    pastaselect

--- src/Pages/Register/register.js
+++ src/Pages/Register/register.js
@@ -3,20 +3,29 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../../Components/Button/button";
 import InputText from "../../Components/input/input";
-//import Select from "../../Components/input/input";
+import Select from "../../Components/Select/select";
 import "./register.css";
-import Img from "../../img/background.jpeg";
+//import Img from "../../img/background.jpeg";
+
 const Register = () => {
   const [verifyInput, setVerifyInput] = useState(false);
   const [verifyPassword, setVerifyPassword] = useState(false);
+  // const routeHistory = useHistory();
+  //const routerRegister = () => {
+  //  routerHistory.push('/');
 
   const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const [userEmail, setUserEmail] = useState("");
-
+  const [userRole, setUserRole] = useState("");
   const passwordRegis = (e) => {
     e.preventDefault();
-    if (userEmail === " " || userPassword === " " || userName === " ") {
+    if (
+      userEmail === " " ||
+      userPassword === " " ||
+      userName === " " ||
+      userRole === ""
+    ) {
       setVerifyInput(true);
     }
     if (userPassword.length < 6) {
@@ -59,7 +68,20 @@ const Register = () => {
               inputOnChange={(event) => setUserPassword(event.target.value)}
               inputClassName="input-form"
             />
-
+            <Select
+              selectName="order"
+              selectValue={userRole}
+              selectOnChange={(event) => setUserRole(event.target.value)}
+              selectClassName="register-option"
+              optionValue1=""
+              optionValue2="salao"
+              optionValue3="cozinha"
+              optionText1="Cargo"
+              optionText2="Garçom"
+              optionText3="Cozinheiro"
+              optionClassName="input-option-select"
+              optionDisabled
+            />
             <Button
               buttonOnClick={(e) => passwordRegis(e)}
               buttonClass="btn-form"
@@ -68,12 +90,17 @@ const Register = () => {
             </Button>
             {verifyPassword ? (
               <div className="erro">
-                <h2>preencha o campo corretamente</h2>
+                <h2>preencha a senha com seis digitos</h2>
               </div>
             ) : null}
             {verifyInput ? (
               <div className="erro">
                 <h2>preencha o campo corretamente</h2>
+              </div>
+            ) : null}
+            {verifyInput ? (
+              <div className="erro">
+                <h2>Preecha todos os campos</h2>
               </div>
             ) : null}
           </form>
