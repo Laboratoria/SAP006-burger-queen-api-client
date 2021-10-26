c8289091abef4a4a161cc564f1e04ca2bf5e3447
Beatriz Ferraz <79225626+bea-ferraz@users.noreply.github.com>
2021-10-26 14:24:58 -0300

    Update login.js

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-//import "./login.css";
+import Button from "../../Components/Button/button";
+import InputText from "../../Components/imput/imput";
+import "./login.css";
+import Logo from '../../img/logo-img-login';
+
 
 const Login = () => {
-  const logar = () => {};
+  const [verifyInput, setVerifyInput] = useState(false);
+  const [verifyPassword, setVerifyPassword] = useState(false);
+
+  const [userPassword, setUserPassword] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
+  const passwordlogin = (e) => {
+    e.preventDefault();
+    if (userEmail === " " || userPassword === " ") {
+      setVerifyInput(true);
+    }
+    if (userPassword.length < 6) {
+      setVerifyPassword(true);
+    } else {
+      console.log("entrou no else");
+    }
+
+    //console.log("cadastrar user")
+  };
 
   return (
-    <>
-      <h1>Login</h1>
 
-      <button type="submit" onClick={logar}>
-        entrar
-      </button>
+    <div className="login-container">
+         <img className="logo-img-login" src={Logo} alt="Logo naruto" ></img>
+        <form className="login-form">
+          <h1 className="text-login">Login</h1>
+          <InputText
+            inputType="text"
+            inputPlaceholder=" Digite seu Email"
+            inputValue={userEmail}
+            inputOnChange={(event) => setUserEmail(event.target.value)}
+          />
+
+          <InputText
+            InputType="password"
+            inputPlaceholder=" Senha"
+            inputValue={userPassword}
+            inputOnChange={(event) => setUserPassword(event.target.value)}
+          />
+
+          <Button
+            buttonOnClick={(e) => passwordlogin(e)}
+            buttonClass="btn-form"
+          >
+            entrar
+          </Button>
+          {verifyPassword ? (
+            <div className="erro">
+              <h2>preencha o campo corretamente</h2>
+            </div>
+          ) : null}
+          {verifyInput ? (
+            <div className="erro">
+              <h2>preencha o campo corretamente</h2>
+            </div>
+          ) : null}
+          
+        </form>
       <Link to="/Cadastro">Cadastre aqui</Link>
-    </>
+   </div>
   );
 };
-
 export default Login;
